8b5119c967c1f6ff0e2f61574d06051309a355fd
Colin Kohler <ckohler@theaiinstitute.com>
2023-11-15 17:20:44 -0500

    Adding checkpoint and logging

--- fail/utils/torch_utils.py
+++ fail/utils/torch_utils.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Dict, Callable, List
 import torch
 import numpy as np
@@ -74,3 +75,17 @@ def dict_apply(
         else:
             result[key] = func(value)
     return result
+
+
+def copy_to_cpu(x):
+    if isinstance(x, torch.Tensor):
+        return x.detach().to("cpu")
+    elif isinstance(x, dict):
+        result = dict()
+        for k, v in x.items():
+            result[k] = copy_to_cpu(v)
+        return result
+    elif isinstance(x, list):
+        return [copy_to_cpu(k) for k in x]
+    else:
+        return copy.deepcopy(x)
