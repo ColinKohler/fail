4ef40dcb2785ff4f7a241be04be50311c8897fc3
Colin Kohler <ckohler@theaiinstitute.com>
2023-12-07 21:44:37 +0000

    task config updates.

--- fail/utils/harmonics.py
+++ fail/utils/harmonics.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 def circular_harmonics(L, theta):
-    B = [torch.tensor([1 / np.sqrt(2 * np.pi)] * theta.size(0)).view(-1, 1)]
+    B = [torch.tensor([1 / np.sqrt(2 * np.pi)] * theta.size(0), device=theta.device).view(-1, 1)]
     for l in range(1, L+1):
         B.append(l * torch.cos(theta) / np.sqrt(np.pi))
         B.append(l * torch.sin(theta) / np.sqrt(np.pi))
